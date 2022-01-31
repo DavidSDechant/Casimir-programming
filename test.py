24ee2a26017e6c07f453ea6849c1151b176013f7
DavidSDechant <dechantdavidsimon@gmail.com>
2022-01-31 13:59:33 +0100

    added comment

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ print("Hello World")
 import numpy as np
 def circumference(r):
     '''
-    this is a home function which calculates the circumference of a circle which has the radius r
+    this is a function which calculates the circumference of a circle of radius r, th
+    .
     '''
     return np.pi*r*2
 def surface(r):
